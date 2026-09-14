aba9d5451a7903762bce3f632141b2a07bd4cbdd
98001yash <yashchauhan.gaya@gmail.com>
2026-09-14 21:33:01 +0530

    PartitionRecovery is working

---
+++ src/main/java/com/buildyourownkafka/client/PartitionLogRecoveryTest.java
@@ -0,0 +1,42 @@
+package com.buildyourownkafka.client;
+
+import com.buildyourownkafka.broker.PartitionLog;
+import com.buildyourownkafka.broker.Record;
+
+import java.nio.file.Path;
+
+public class PartitionLogRecoveryTest {
+
+    public static void main(String[] args) {
+
+        Path logFile =
+                Path.of("data", "test-topic", "partition-0", "partition.log");
+
+        System.out.println("=== PROCESS 2: RECOVERY ===");
+
+        PartitionLog log = new PartitionLog(logFile);
+
+        Record record0 = log.read(0);
+        Record record1 = log.read(1);
+        Record record2 = log.read(2);
+
+        printRecord(record0);
+        printRecord(record1);
+        printRecord(record2);
+    }
+
+    private static void printRecord(Record record) {
+
+        if (record == null) {
+            System.out.println("Record not found");
+            return;
+        }
+
+        System.out.println(
+                "Recovered offset "
+                        + record.offset()
+                        + " -> "
+                        + new String(record.value())
+        );
+    }
+}
